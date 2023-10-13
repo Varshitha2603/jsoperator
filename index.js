8826652947afc2622b07a2b4001c25f2dc8d972e
DCKAP <varshithaksdckap@gmail.com>
2023-10-13 14:40:54 +0530

    operator

---
+++ index.js
@@ -0,0 +1,115 @@
+// Convert age into days
+// let age, age1, age2;
+// age=calcAge(65);
+// age1=calcAge(0);
+// age2=calcAge(20);
+// function calcAge(x)
+// {
+//     return x*365;
+// }
+// console.log("Convert age into days" +  " " + age + "days" + "," + age1 + "days" + "," + age2 + "days.");
+// ---------------------------------------------------------------------------------------------------------------------------
+// Less than 100
+// let sum, sum1, sum2;
+// sum=lessThan100(22,15);
+// sum1=lessThan100(83,34);
+// sum2=lessThan100(3,77);
+// function lessThan100(x,y,z)
+{
+    // return (x+y)<100; 
+}
+// console.log("Less than 100:" + " " + sum + "," + sum1 + "," + sum2);
+// ---------------------------------------------------------------------------------------------------------------------------
+// Convert hours into seconds
+// let hours,hours1,hours2;
+// hours=howManySeconds(2);
+// hours1=howManySeconds(10);
+// hours2=howManySeconds(24);
+// function howManySeconds(a)
+// {
+    // 1 hours = 3600 seconds
+    // return a*3600;
+// }
+// console.log("Convert hours into seconds" + " " + hours + "," + hours1 + "," + hours2);
+// ---------------------------------------------------------------------------------------------------------------------------
+// Sum of Polygon Angles
+// let angles,angles1,angles2;
+// angles=Polygon(3);
+// angles1=Polygon(4);
+// angles2=Polygon(6);
+// function Polygon(v)
+// {
+//     return v>2 ? (v-2)*180 : false;
+// }
+// console.log("Sum of Polygon Angles" + " " + angles + "," + angles1 + "," + angles2);
+// ---------------------------------------------------------------------------------------------------------------------------
+// Return Something to Me!
+// let b="Something" + " ";
+// let word,word1,word2;
+// word=something("is better than nothing");
+// word1=something("Bob Jane")
+// word2=something("Something")
+// function something(a) {
+//     return b += a;
+// }
+// console.log("Return Something to Me! : " + word + "," + word1 + "," + word2);
+// ------------------------------------------------------------------------------------------------------------------------
+// Is the Number Less Than or equal to zero
+// let less,less1,less2;
+// less=equalTozero(5);
+// less1=equalTozero(0);
+// less2=equalTozero(-2);
+// function equalTozero(g)
+// {
+//     return g<=0 || g<=0;
+// }
+// console.log("Is the Number Less Than or equal to zero : " + less + "," + less1 + "," + less2);
+// --------------------------------------------------------------------------------------------------------------------------
+// Using the "&&" operator
+// let operator,operator1,operator2;
+// operator=and(true,false);
+// operator1=and(true,true);
+// operator2=and(false,true);
+// operator3=and(false,false);
+// function and(x,y) 
+// {
+//     return x && y ? true : false;
+// }
+// console.log("Using the && operator" + " " + operator + "," + operator1 + "," + operator2 + "," + operator3);
+// ----------------------------------------------------------------------------------------------------------------------------
+// The Form Problems
+// let chicken=2;
+// let cow=4;
+// let pig=4;
+// let leg,leg1,leg2; 
+// leg=animal(2,3,5);
+// leg1=animal(1,2,3);
+// leg2=animal(5,2,8);
+// function animal(a,b,c)
+// {
+//     return (a*chicken)+(b*cow)+(c*pig);
+// }
+// console.log("The Form Problems : " + leg + "," + leg1 + "," + leg2);
+// -----------------------------------------------------------------------------------------------------------------------------
+// Equality Check
+// let equal, equal1,equal2;
+// equal=checkEquality(1,true);
+// equal1=checkEquality(0,"0");
+// equal2=checkEquality(1,1);
+// function checkEquality(x,y)
+// {
+//     return x === y ? true : false;
+// }
+// console.log("Equality Check : " + equal + "," + equal1 + "," + equal2);
+// --------------------------------------------------------------------------------------------------------------------------
+// Fix the Expression
+// let e=d=c=7;
+// let code;
+// code=isSeven(4);
+// cde11=isSeven(9);
+// cde12=isSeven(7);
+// function isSeven(y)
+// {
+//     return e==y, d==y, c==y;
+// }
+// console.log("Fix the Expression : " + code + "," + cde11 + "," + cde12);
